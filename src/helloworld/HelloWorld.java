881bfe8e46dcd3533520f9bbf546cf9ba2153bc8
karstenmonteiro <karstenmonteiro@kmLaptop>
2022-09-13 16:45:32 -0600

    Week 2 Lab Created a new Java Application, HelloWorld, for a practice submission.

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+/**
+ * HelloWorld
+ * 
+ * @author karstenmonteiro
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("This is a practice submission for Week02Lab.");
+    }
+}
