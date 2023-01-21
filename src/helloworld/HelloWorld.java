d57dbc07d205a7c3820f0b8529e388cd45dffe68
karstenmonteiro <karstenmonteiro@kmLaptop>
2023-01-21 03:08:15 -0700

    Week 2 Lab: HelloWorld (initial commit) Created a simple Java Application, HelloWorld, for the week 2 lab.

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,18 @@
+package helloworld;
+
+/**
+ * Week 2 Lab:
+ *  HelloWorld
+ * 
+ * @author karstenmonteiro
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        
+        System.out.println("");
+        System.out.println("Hello world!");
+        System.out.println("This is a practice submission for the week 2 lab.");
+        System.out.println("Bye :)");
+        System.out.println("");
+    }
+}
